3da38d34b916aed177bd2c49814b6adc0bd4b3d5
Developer Shohan <dev.shohanur@gmail.com>
2023-12-29 00:19:28 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<div className="todo-wrapper">
+  <div className="todo-container">
+    <div className="todo-header">
+
+      <h1 className="text-3xl font-bold  uppercase">Todo list</h1>
+    </div>
+    <div className="todo-body">
+      
+    </div>
+
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
