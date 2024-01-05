bfbfe4d1a7fd1c91eae2a1c67ef2e5d9d14d58a5
Developer Shohan <dev.shohanur@gmail.com>
2024-01-05 23:17:46 +0600

    login reg router added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,7 @@
 import { createUserWithEmailAndPassword } from "firebase/auth"
 
 import { auth } from "../../firebase.js"
-import { useNavigate,Link } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import UserContext from "../../context/UserContext.js"
 import { useContext } from "react"
 
@@ -9,7 +9,7 @@ import { useContext } from "react"
 const Register = () => {
     const navigate = useNavigate()
 
-const {input, setInput} = useContext(UserContext)
+    const { input, setInput } = useContext(UserContext)
     const handleInputChange = (e) => {
         setInput((prevState) => ({
             ...prevState,
@@ -20,14 +20,14 @@ const {input, setInput} = useContext(UserContext)
         e.preventDefault()
         createUserWithEmailAndPassword(auth, input.email, input.password).then(() => {
             navigate("/login")
-        }).catch(err=> console.log(err.massage))
+        }).catch(err => console.log(err.massage))
 
     }
 
     return (
         <div>
 
-            <div className=" border rounded-lg border-blue-100" >
+            <div className=" border bg-white rounded-lg border-blue-100" >
                 <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-6 lg:px-8">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                         <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -56,7 +56,7 @@ const {input, setInput} = useContext(UserContext)
                                     autoComplete="email"
                                     placeholder='Email Address'
                                     required
-                                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full bg-transparent rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     onChange={handleInputChange}
                                     value={input.email}
                                 />
@@ -69,14 +69,14 @@ const {input, setInput} = useContext(UserContext)
                                     autoComplete="current-password"
                                     placeholder='Password'
                                     required
-                                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full bg-transparent rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     onChange={handleInputChange}
                                     value={input.password}
                                 />
                             </div>
                             <div>
                                 <button
-                                    type="submit"
+                                    type="button"
                                     className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     Sign in
